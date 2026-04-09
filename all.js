a7a342c5663471658356a047e6c330e026518a62
wusnlin-pixel <wusnlin@gmail.com>
2026-04-09 23:53:18 +0800

    feat: update GCP configuration with client ID, API key, and spreadsheet ID

--- all.js
+++ all.js
@@ -11,11 +11,12 @@
 // --- 設定 ---
 const CONFIG = {
     // 請填入您的 GCP Client ID
-    CLIENT_ID: '',
+    CLIENT_ID: '768071495555-eltu7kj8of1qr6psqpqia3o4f6rpsj4j.apps.googleusercontent.com',
     // 請填入您的 GCP API Key
-    API_KEY: '',
+    API_KEY: 'AIzaSyBt5RhmXt9TdfSMJxR-R3NI4ChqBmpqzAM
+',
     // 請填入您的 Google Sheet ID
-    SPREADSHEET_ID: '',
+    SPREADSHEET_ID: '13NpYfhHM7LDV_vBUrp072u7y_cjmcRbtqAAJGuh-CwI',
 
     // Google Sheets Discovery Doc
     DISCOVERY_DOC: 'https://sheets.googleapis.com/$discovery/rest?version=v4',
